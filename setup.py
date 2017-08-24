b93eb1fff1b6c64b0cb57af1bd05c1ed2047cb56
Tammo Ippen <tammo.ippen@posteo.de>
2017-08-24 13:33:39 +0200

    Bump version number.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='geopip',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=[
         'geohash2',
@@ -17,7 +17,7 @@ setup(
     author_email='tammo.ippen@posteo.de',
     description='Reverse geocode a lng/lat coordinate within a geojson FeatureCollection.',
     url='https://github.com/tammoippen/geopip',
-    download_url='https://github.com/tammoippen/geopip/archive/0.1.tar.gz',
+    download_url='https://github.com/tammoippen/geopip/archive/v0.2.tar.gz',
     keywords=['geojson', 'point in polygon', 'reverse geocode', 'countries'],
     include_package_data=True,
 )
