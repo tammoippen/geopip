bbadef8eac82b1a7371846c713aa919ae1634b27
Tammo Ippen <tammo.ippen@posteo.de>
2017-09-20 18:03:26 +0200

    Test pypy versions aswell
    add badges and setup.py informations + coverall

--- setup.py
+++ setup.py
@@ -26,7 +26,20 @@ setup(
     description='Reverse geocode a lng/lat coordinate within a geojson FeatureCollection.',
     long_description=long_description,
     url='https://github.com/tammoippen/geopip',
+    license='MIT',
     download_url='https://github.com/tammoippen/geopip/archive/v{}.tar.gz'.format(version),
     keywords=['geojson', 'point in polygon', 'reverse geocode', 'countries'],
     include_package_data=True,
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ]
 )
